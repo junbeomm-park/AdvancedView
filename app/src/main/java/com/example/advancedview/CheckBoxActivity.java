4c522cb86a337feb3ba4dc94018f2cbd7eba3bdc
park junbeom <junbum0674@gmail.com>
2021-11-03 09:53:29 +0900

    21.11.03 AdvancedView

---
+++ app/src/main/java/com/example/advancedview/CheckBoxActivity.java
@@ -0,0 +1,14 @@
+package com.example.advancedview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CheckBoxActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.check);
+    }
+}
