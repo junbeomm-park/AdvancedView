b22b1117535f1d2fc875418273e44b769fc3d911
park junbeom <junbum0674@gmail.com>
2021-11-03 14:41:41 +0900

    21.11.03 AdvancedView Drawable

---
+++ app/src/main/java/com/example/advancedview/ViewTestActivity.java
@@ -0,0 +1,14 @@
+package com.example.advancedview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.image_test2);
+    }
+}
